90fb33484f877b579f3ee2108c3c7fb989519d63
ENigmusSS <128493758+ENigmusSS@users.noreply.github.com>
2025-02-13 22:05:04 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,28 @@
+'use strict';
+
 /**
  * @param {number} days
  *
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE = 40;
+  const BASIC_DISCOUNT = 20;
+  const BASIC_DAYS = 3;
+  const EXTRA_DISCOUNT = 50;
+  const EXTRA_DAYS = 7;
+
+  const fullcost = days * PRICE;
+
+  if (days >= EXTRA_DAYS) {
+    return fullcost - EXTRA_DISCOUNT;
+  }
+
+  if (days >= BASIC_DAYS) {
+    return fullcost - BASIC_DISCOUNT;
+  }
+
+  return fullcost;
 }
 
 module.exports = calculateRentalCost;
